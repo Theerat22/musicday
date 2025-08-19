a37bf14aa4ff1ce5e4283bdfa82501fec66218f2
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2025-08-19 23:07:46 +0700

    redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <section className="min-h-screen bg-white mt-4">
       <Products />
       <footer className="bg-blue-950 p-1">
-        <p className="text-center p-3 text-white">
+        <p className="text-center text-sm p-3 text-white">
           &copy; CD Student Council 2025 x ชุมนุมจัดดอกไม้ x ชุมนุม Do It Yourself
         </p>
       </footer>
