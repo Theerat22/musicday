b966cf57420bdf19cff6c865ab1277cb1e977a5f
Theeratdolchat Chatchai <92778404+Theerat22@users.noreply.github.com>
2025-09-01 08:21:27 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 // import Products from "./components/Products";
-import Products from "./components/Test";
 
 export default function Home() {
   return (
     <section className="min-h-screen bg-white mt-4">
-      <Products />
+      <p>ปิดรับออเดอร์</p>
       <footer className="bg-blue-950 p-1">
         <p className="text-center text-sm p-3 text-white">
           &copy; CHITRALADA 56
