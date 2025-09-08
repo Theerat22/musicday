4d2df2a9af6ef227b532ee8b054de092ab7f611d
Theeratdolchat Chatchai <92778404+Theerat22@users.noreply.github.com>
2025-09-08 14:06:28 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-// import Products from "./components/Products";
+import Products from "./components/Products";
 
 export default function Home() {
   return (
     <section className="min-h-screen bg-white flex flex-col justify-center text-center ">
-      <p className="font-bold text-xl">ปิดรับออเดอร์แล้ว</p>
-      <p className="text-lg mt-3">เจอกันในงานมิวสิคเดย์น้าาา</p>
+      <Products />
     </section>
   );
 }
